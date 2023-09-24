bd35ef05d64b1d301b7b10ae65dd7afa5eb21e90
GeometryMay <moisesmaycaamal.mmc@gmail.com>
2023-09-23 20:19:13 -0500

    Se actualizo la interfaz del Windows Form.

--- SumaDecimalesApp/Form1.Designer.cs
+++ SumaDecimalesApp/Form1.Designer.cs
@@ -28,109 +28,261 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtNumero1 = new System.Windows.Forms.TextBox();
-            this.txtNumero2 = new System.Windows.Forms.TextBox();
-            this.btnSumar = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblResultado = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.txtNumero1 = new Guna.UI.WinForms.GunaTextBox();
+            this.txtNumero2 = new Guna.UI.WinForms.GunaTextBox();
+            this.gunaButton1 = new Guna.UI.WinForms.GunaButton();
+            this.gunaButton2 = new Guna.UI.WinForms.GunaButton();
+            this.gunaPictureBox1 = new Guna.UI.WinForms.GunaPictureBox();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel3 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel4 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel5 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel6 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel7 = new Guna.UI.WinForms.GunaLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // txtNumero1
-            // 
-            this.txtNumero1.Location = new System.Drawing.Point(12, 38);
-            this.txtNumero1.Name = "txtNumero1";
-            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
-            this.txtNumero1.TabIndex = 0;
-            this.txtNumero1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // txtNumero2
-            // 
-            this.txtNumero2.Location = new System.Drawing.Point(12, 78);
-            this.txtNumero2.Name = "txtNumero2";
-            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
-            this.txtNumero2.TabIndex = 1;
-            this.txtNumero2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // btnSumar
-            // 
-            this.btnSumar.Location = new System.Drawing.Point(28, 122);
-            this.btnSumar.Name = "btnSumar";
-            this.btnSumar.Size = new System.Drawing.Size(75, 23);
-            this.btnSumar.TabIndex = 2;
-            this.btnSumar.Text = "Sumar";
-            this.btnSumar.UseVisualStyleBackColor = true;
-            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
             // 
             // lblResultado
             // 
             this.lblResultado.AutoSize = true;
-            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblResultado.Location = new System.Drawing.Point(129, 56);
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(156, 144);
             this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(0, 24);
+            this.lblResultado.Size = new System.Drawing.Size(0, 25);
             this.lblResultado.TabIndex = 3;
             // 
-            // button1
+            // gunaElipse1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 160);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Restar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.gunaElipse1.TargetControl = this;
             // 
-            // button2
+            // txtNumero1
             // 
-            this.button2.Location = new System.Drawing.Point(28, 199);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Dividir";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.txtNumero1.BackColor = System.Drawing.Color.Transparent;
+            this.txtNumero1.BaseColor = System.Drawing.Color.Transparent;
+            this.txtNumero1.BorderColor = System.Drawing.Color.Transparent;
+            this.txtNumero1.BorderSize = 1;
+            this.txtNumero1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNumero1.FocusedBaseColor = System.Drawing.Color.Transparent;
+            this.txtNumero1.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.txtNumero1.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtNumero1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero1.Location = new System.Drawing.Point(72, 83);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.PasswordChar = '\0';
+            this.txtNumero1.Radius = 5;
+            this.txtNumero1.Size = new System.Drawing.Size(94, 32);
+            this.txtNumero1.TabIndex = 7;
+            this.txtNumero1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNumero1.TextChanged += new System.EventHandler(this.txtNumero1_TextChanged);
             // 
-            // button3
+            // txtNumero2
             // 
-            this.button3.Location = new System.Drawing.Point(28, 241);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Limpiar";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.txtNumero2.BackColor = System.Drawing.Color.Transparent;
+            this.txtNumero2.BaseColor = System.Drawing.Color.Transparent;
+            this.txtNumero2.BorderColor = System.Drawing.Color.Transparent;
+            this.txtNumero2.BorderSize = 1;
+            this.txtNumero2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNumero2.FocusedBaseColor = System.Drawing.Color.Transparent;
+            this.txtNumero2.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.txtNumero2.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtNumero2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero2.Location = new System.Drawing.Point(202, 83);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.PasswordChar = '\0';
+            this.txtNumero2.Radius = 5;
+            this.txtNumero2.Size = new System.Drawing.Size(94, 32);
+            this.txtNumero2.TabIndex = 8;
+            this.txtNumero2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // gunaButton1
+            // 
+            this.gunaButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaButton1.AnimationSpeed = 0.03F;
+            this.gunaButton1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaButton1.ForeColor = System.Drawing.Color.White;
+            this.gunaButton1.Image = null;
+            this.gunaButton1.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaButton1.Location = new System.Drawing.Point(83, 205);
+            this.gunaButton1.Name = "gunaButton1";
+            this.gunaButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.gunaButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaButton1.OnHoverImage = null;
+            this.gunaButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaButton1.Radius = 5;
+            this.gunaButton1.Size = new System.Drawing.Size(83, 42);
+            this.gunaButton1.TabIndex = 10;
+            this.gunaButton1.Text = "Restar";
+            this.gunaButton1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.gunaButton1.Click += new System.EventHandler(this.gunaButton1_Click);
+            // 
+            // gunaButton2
+            // 
+            this.gunaButton2.AnimationHoverSpeed = 0.07F;
+            this.gunaButton2.AnimationSpeed = 0.03F;
+            this.gunaButton2.BackColor = System.Drawing.Color.Transparent;
+            this.gunaButton2.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaButton2.BorderColor = System.Drawing.Color.Black;
+            this.gunaButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaButton2.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaButton2.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaButton2.ForeColor = System.Drawing.Color.White;
+            this.gunaButton2.Image = null;
+            this.gunaButton2.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaButton2.Location = new System.Drawing.Point(202, 205);
+            this.gunaButton2.Name = "gunaButton2";
+            this.gunaButton2.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.gunaButton2.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaButton2.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaButton2.OnHoverImage = null;
+            this.gunaButton2.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaButton2.Radius = 5;
+            this.gunaButton2.Size = new System.Drawing.Size(83, 42);
+            this.gunaButton2.TabIndex = 11;
+            this.gunaButton2.Text = "Limpiar";
+            this.gunaButton2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.gunaButton2.Click += new System.EventHandler(this.gunaButton2_Click);
+            // 
+            // gunaPictureBox1
+            // 
+            this.gunaPictureBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaPictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("gunaPictureBox1.Image")));
+            this.gunaPictureBox1.Location = new System.Drawing.Point(355, -1);
+            this.gunaPictureBox1.Name = "gunaPictureBox1";
+            this.gunaPictureBox1.Size = new System.Drawing.Size(28, 26);
+            this.gunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gunaPictureBox1.TabIndex = 12;
+            this.gunaPictureBox1.TabStop = false;
+            this.gunaPictureBox1.Click += new System.EventHandler(this.gunaPictureBox1_Click);
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel1.Location = new System.Drawing.Point(127, 17);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(125, 32);
+            this.gunaLabel1.TabIndex = 13;
+            this.gunaLabel1.Text = "MANTISA";
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel2.Location = new System.Drawing.Point(137, 274);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(84, 25);
+            this.gunaLabel2.TabIndex = 14;
+            this.gunaLabel2.Text = "Equipo 1";
+            // 
+            // gunaLabel3
+            // 
+            this.gunaLabel3.AutoSize = true;
+            this.gunaLabel3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel3.Location = new System.Drawing.Point(88, 297);
+            this.gunaLabel3.Name = "gunaLabel3";
+            this.gunaLabel3.Size = new System.Drawing.Size(183, 21);
+            this.gunaLabel3.TabIndex = 15;
+            this.gunaLabel3.Text = "Yael Moises May Caamal";
+            // 
+            // gunaLabel4
+            // 
+            this.gunaLabel4.AutoSize = true;
+            this.gunaLabel4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel4.Location = new System.Drawing.Point(146, 258);
+            this.gunaLabel4.Name = "gunaLabel4";
+            this.gunaLabel4.Size = new System.Drawing.Size(61, 15);
+            this.gunaLabel4.TabIndex = 16;
+            this.gunaLabel4.Text = "Create by:";
+            this.gunaLabel4.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // gunaLabel5
+            // 
+            this.gunaLabel5.AutoSize = true;
+            this.gunaLabel5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel5.Location = new System.Drawing.Point(70, 318);
+            this.gunaLabel5.Name = "gunaLabel5";
+            this.gunaLabel5.Size = new System.Drawing.Size(216, 21);
+            this.gunaLabel5.TabIndex = 17;
+            this.gunaLabel5.Text = "Raúl Omar Martínez Armenta";
+            // 
+            // gunaLabel6
+            // 
+            this.gunaLabel6.AutoSize = true;
+            this.gunaLabel6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel6.Location = new System.Drawing.Point(68, 339);
+            this.gunaLabel6.Name = "gunaLabel6";
+            this.gunaLabel6.Size = new System.Drawing.Size(225, 21);
+            this.gunaLabel6.TabIndex = 18;
+            this.gunaLabel6.Text = "Edison Arquímides García Pech";
+            // 
+            // gunaLabel7
+            // 
+            this.gunaLabel7.AutoSize = true;
+            this.gunaLabel7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel7.Location = new System.Drawing.Point(75, 360);
+            this.gunaLabel7.Name = "gunaLabel7";
+            this.gunaLabel7.Size = new System.Drawing.Size(204, 21);
+            this.gunaLabel7.TabIndex = 19;
+            this.gunaLabel7.Text = "José Ricardo Notario García";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(666, 323);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.lblResultado);
-            this.Controls.Add(this.btnSumar);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(385, 387);
+            this.Controls.Add(this.gunaLabel7);
+            this.Controls.Add(this.gunaLabel6);
+            this.Controls.Add(this.gunaLabel5);
+            this.Controls.Add(this.gunaLabel4);
+            this.Controls.Add(this.gunaLabel3);
+            this.Controls.Add(this.gunaLabel2);
+            this.Controls.Add(this.gunaLabel1);
+            this.Controls.Add(this.gunaPictureBox1);
+            this.Controls.Add(this.gunaButton2);
+            this.Controls.Add(this.gunaButton1);
             this.Controls.Add(this.txtNumero2);
             this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblResultado);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtNumero1;
-        private System.Windows.Forms.TextBox txtNumero2;
-        private System.Windows.Forms.Button btnSumar;
         private System.Windows.Forms.Label lblResultado;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private Guna.UI.WinForms.GunaElipse gunaElipse1;
+        private Guna.UI.WinForms.GunaTextBox txtNumero1;
+        private Guna.UI.WinForms.GunaTextBox txtNumero2;
+        private Guna.UI.WinForms.GunaButton gunaButton2;
+        private Guna.UI.WinForms.GunaButton gunaButton1;
+        private Guna.UI.WinForms.GunaPictureBox gunaPictureBox1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel7;
+        private Guna.UI.WinForms.GunaLabel gunaLabel6;
+        private Guna.UI.WinForms.GunaLabel gunaLabel5;
+        private Guna.UI.WinForms.GunaLabel gunaLabel4;
+        private Guna.UI.WinForms.GunaLabel gunaLabel3;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
     }
 }
 
--- SumaDecimalesApp/Form1.cs
+++ SumaDecimalesApp/Form1.cs
@@ -20,31 +20,21 @@ namespace SumaDecimalesApp
 
         private void btnSumar_Click(object sender, EventArgs e)
         {
-            try
-            {
-                // Leer los números ingresados por el usuario
-                decimal numero1 = decimal.Parse(txtNumero1.Text, CultureInfo.InvariantCulture);
-                decimal numero2 = decimal.Parse(txtNumero2.Text, CultureInfo.InvariantCulture);
-
-                // Calcular la suma sin redondear
-                decimal suma = numero1 + numero2;
-
-                // Mostrar la suma en el cuadro de texto de resultado sin redondear
-                lblResultado.Text = suma.ToString("0.#############################", CultureInfo.InvariantCulture);
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Por favor, ingrese números decimales válidos.");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ha ocurrido un error: " + ex.Message);
-            }
-
-
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void gunaButton1_Click(object sender, EventArgs e)
         {
             try
             {
@@ -68,35 +58,23 @@ namespace SumaDecimalesApp
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                // Leer los números ingresados por el usuario
-                decimal numero1 = decimal.Parse(txtNumero1.Text, CultureInfo.InvariantCulture);
-                decimal numero2 = decimal.Parse(txtNumero2.Text, CultureInfo.InvariantCulture);
-
-                // Calcular la suma sin redondear
-                decimal division = numero1 / numero2;
-
-                // Mostrar la suma en el cuadro de texto de resultado sin redondear
-                lblResultado.Text = division.ToString("0.#############################", CultureInfo.InvariantCulture);
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Por favor, ingrese números decimales válidos.");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ha ocurrido un error: " + ex.Message);
-            }
-        }
-
-        private void button3_Click(object sender, EventArgs e)
+        private void gunaButton2_Click(object sender, EventArgs e)
         {
             txtNumero1.Text = "";
             txtNumero2.Text = "";
             lblResultado.Text = "";
+            txtNumero1.Focus();
+            
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void gunaPictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
